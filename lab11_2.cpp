a821785632e7fe439eedad5c99347271b27a8bfb
Wia <chawis_jirath@cmu.ac.th>
2025-12-26 15:20:51 +0700

    lab11 1

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -22,7 +22,7 @@ int findYugiAction(int s){
 	else{
 		// If current score is 6,7,8, Yugi will draw with probability 69% and will stay with probability 31% 
 		// Write conditions here using random number 
-	}
+	}	
 }
 
 void checkWinner(int p, int y){
